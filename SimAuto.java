23e90e778678277eca2ac1fd0abeae0b5e8de646
Edwin Raj A <125364635+EdwinRaj14@users.noreply.github.com>
2023-03-13 12:29:56 +0530

    Update SimAuto.java

--- SimAuto.java
+++ SimAuto.java
@@ -1,26 +1,20 @@
-import Utilities.Excelread;
-import org.apache.commons.mail.EmailException;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
-import java.util.Locale;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+
 
 public class SimAuto {
     public static String userName;
@@ -37,33 +31,40 @@ public class SimAuto {
     public static int endDate;
     public static int endMonth;
     public static int endYear;
-    public static String start= "2/2/2023";
-    public static String end= "2/24/2023";
+    public static String start = "02/01/2023";
+    public static String end = "03/13/2023";
+    public static String drive= "chrome";
 
-    public static void main(String args[]) throws AWTException, IOException {
+
+
+    static Excelread read = new Excelread();
+
+    public static void main(String args[]) throws IOException, AWTException {
+        read.deleteExcelFile();
         getValuesforLogin();
-        simStatus();
+        // simStatus();
         simPriority();
         startDateSelection();
         endDateSelection();
         urlLaunch();
-        statusSelection();
+        //  statusSelection();
         SimPrioritySelection();
         dateClick();
         downloadExcel();
-        Excelread read = new Excelread();
         read.excelRead();
+        read.mailReport();
         scanner.close();
     }
 
     public static void getValuesforLogin() {
         // System.out.println("Enter Your userName without @amazon.com");
         // userName = scanner.next();
-        userName="edwinaz";
+        userName = "edwinaz";
         // System.out.println("Enter Your Yuibikey pin");
         // password = scanner.next();
-        password="411013103006";
+        password = "411013103006";
     }
+
     public static void startDateSelection() {
         /*System.out.println("Enter the start date you want 01 to 31");
         startDate = scanner.nextInt();
@@ -80,9 +81,9 @@ public class SimAuto {
         System.out.println("Enter the start year you want");
         startYear = scanner.nextInt();
         System.out.println(startDate + "/" + startMonth + "/" + startYear);*/
-        startDate=2;
-        startMonth=2;
-        startYear=2023;
+        startDate = 2;
+        startMonth = 2;
+        startYear = 2023;
     }
 
     public static void endDateSelection() {
@@ -104,13 +105,33 @@ public class SimAuto {
         System.out.println("0" + endDate + "/" + endMonth + "/" + endYear);
         */
     }
-    public static void urlLaunch() {
+
+    public static void urlLaunch() throws MalformedURLException {
         //Webpage initilization
-        driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        if (drive=="chrome") {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+          //  DesiredCapabilities cp = new DesiredCapabilities();
+            //cp.setCapability(ChromeOptions.CAPABILITY,options);
+            //options.merge(cp);
+            //options.setAcceptInsecureCerts(true);
+            driver = new ChromeDriver(options);
+        }
+        else {
+            driver= new FirefoxDriver();
+            System.setProperty("webdriver.gecko.driver", "geckodriver");
+        }
         driver.manage().window().maximize();
-        driver.get("https://tinyurl.com/2kegyhge");
-        //Entering user name and password for sim
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+       driver.get("https://tinyurl.com/bdf5cfd5");
+        //Entering user name and password for
+       try{ Thread.sleep(1000);}
+       catch (Exception e)
+       {
+           e.printStackTrace();
+       }
         driver.findElement(By.id("user_name_field")).sendKeys(userName);
         driver.findElement(By.id("user_name_btn")).click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -120,6 +141,7 @@ public class SimAuto {
 
     //clicking status for sim
     public static void statusSelection() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.id("s2id_filter-dropdown")).click();
         WebElement status = driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul/li[41]"));
         // Javascript executor
@@ -167,13 +189,19 @@ public class SimAuto {
         } else if (priority == 7) {
             simPriority = "Low&Medium&High";
         } else {
-            System.out.println("Enter value between 1 to 3");
+            System.out.println("Enter value between 1 to 7");
             simPriority();
         }
         System.out.println("you selected " + simPriority + " sim");
     }
 
     public static void SimPrioritySelection() {
+        try{ Thread.sleep(60000);}
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.id("s2id_filter-dropdown")).click();
         WebElement priorities = driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul/li[34]/div"));
         // Javascript executor
@@ -217,7 +245,6 @@ public class SimAuto {
     }
 
 
-
     public static void dateClick() {
         driver.findElement(By.id("s2id_filter-dropdown")).click();
         WebElement dates = driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul/li[11]/div"));
@@ -225,14 +252,15 @@ public class SimAuto {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", dates);
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul/li[11]/div")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[4]/div/div[1]/span/span/i")).click();
-       // driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(startMonth + "/" + startDate + "/" + startYear);
+        // driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(startMonth + "/" + startDate + "/" + startYear);
         driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(start);
         driver.findElement(By.xpath("//input[@type='text']")).sendKeys(Keys.TAB);
-      //  driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys(endMonth + "/" + endDate + "/" + endYear);
+        //  driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys(endMonth + "/" + endDate + "/" + endYear);
         driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys(end);
         driver.findElement(By.xpath("//button[@data-name='add-search-filter-button']")).click();
         driver.findElement(By.id("initiate-search")).click();
     }
+
     public static void downloadExcel() throws AWTException {
         driver.findElement(By.xpath("//button[@data-toggle='dropdown']")).click();
         driver.findElement(By.xpath("//a[@data-name='export-search-results']")).click();
@@ -244,6 +272,5 @@ public class SimAuto {
         robot.keyPress(KeyEvent.VK_ENTER); //press enter key
         robot.keyRelease(KeyEvent.VK_ENTER); //release enter key
     }
-
 }
 
